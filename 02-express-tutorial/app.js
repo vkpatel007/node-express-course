fab00b43092aab280d1af3bec5980ff13d7b225e
vkpatel007 <vkpatel007@gmail.com>
2023-03-26 16:27:54 -0400

    laptop 3-36 update

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,48 +1,23 @@
 const express = require('express')
-const { products } = require('./data')
 const app = express();
-
+const logger = require('./logger')
+const authorize = require('./authorize')
+app.use([authorize, logger])
 
 app.get('/', (req, res) => {
-  res.send('<h1> Home Page </h1><a href="/api/products">products</a>')
+  res.send('Home')
+})
+app.get('/about', (req, res) => {
+  res.send('About')
 })
 
-app.get('/api/products', (req, res) => {
-  const newProducts = products.map(product => {
-    const { id, name, image } = product;
-    return { id, name, image }
-  });
-  res.json(newProducts)
+app.get('/about', (req, res) => {
+  res.send('About')
 })
 
-app.get('/api/products/:productID', (req, res) => {
-  //console.log(req)
-  //console.log(req.params)
-  const { productID } = req.params;
-  const singleProduct = products.find((product) => product.id === Number(productID))
-
-  if (!singleProduct) {
-    return res.status(404).send('Product Does Not Exist')
-  } else return res.json(singleProduct)
+app.get('/api/items', (req, res) => {
+  res.send('About')
 })
-
-app.get('/api/v1/query', (req, res) => {
-  console.log(req.query);
-  const { search, limit } = req.query
-  let sortedProducts = [...products]
-
-  if (search) {
-    sortedProducts = sortedProducts.filter(product => {
-      return product.name.startsWith(search)
-    })
-  }
-
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit))
-  }
-  res.status(200).json(sortedProducts)
-})
-
 
 app.listen(5000, () => {
   console.log(`Server is listening on port 5000....`)
