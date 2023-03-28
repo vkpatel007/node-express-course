cf25ad59732d4ed90533c84c975b032cfd09d480
vkpatel007 <vkpatel007@gmail.com>
2023-03-27 20:30:44 -0400

    Mar-27th Main PC

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,15 +1,12 @@
-require('./DB/connect')
+
 const express = require('express')
 const app = express();
 const tasks = require('./routes/tasks')
 const connectDB = require('./DB/connect')
 require('dotenv').config()
 
+app.use(express.static('./public'))
 app.use(express.json())
-
-app.get('/hello', (req, res) => {
-  res.send('Task Manager App')
-})
 
 app.use('/api/v1/tasks', tasks)
 
