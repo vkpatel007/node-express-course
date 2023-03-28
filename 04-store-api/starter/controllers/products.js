8485a22ba6aaf0457c0fc1a7fc88875c574169a3
vkpatel007 <vkpatel007@gmail.com>
2023-03-28 05:31:34 -0400

    3-28 laptop night

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -3,13 +3,13 @@ const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
 
-  const products = await Product.find({}).sort('-name')
+  const products = await Product.find({}).select('name price')
   res.status(200).json({ products, nbHits: products.length })
 }
 
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query
+  const { featured, company, name, sort, fields } = req.query
   const queryObject = {}
 
   if (featured) {
@@ -23,12 +23,19 @@ const getAllProducts = async (req, res) => {
   }
   // console.log(queryObject)
   let result = Product.find(queryObject)
+  //sort
   if (sort) {
     const sortList = sort.split(',').join(' ')
     result = result.sort(sortList)
   } else {
     result = result.sort('createAt')
   }
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ')
+    result = result.select(fieldsList)
+  }
+
+
   const products = await result
   res.status(200).json({ products, nbHits: products.length })
 }
