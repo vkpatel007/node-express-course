d0dd31a87013a1e9ee977ff44873370d01cd7e9b
vkpatel007 <vkpatel007@gmail.com>
2023-03-28 03:23:31 -0400

    3-28 Main PC early night

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -4,19 +4,22 @@ const app = express();
 const tasks = require('./routes/tasks')
 const connectDB = require('./DB/connect')
 require('dotenv').config()
+const notFound = require('./middleware/not-found')
+const errorHandler = require('./middleware/error-handler')
 
 app.use(express.static('./public'))
 app.use(express.json())
 
 app.use('/api/v1/tasks', tasks)
+app.use(notFound)
+app.use(errorHandler)
 
-
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI)
-    app.listen(port, console.log(`server is listening on port ${port}....`))
+    app.listen(port, () => console.log(`server is listening on port ${port}....`))
 
   } catch (error) {
     console.log(error)
