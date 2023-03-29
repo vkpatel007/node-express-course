3ec5950b759062c3628b2bedb51503162cadcf64
vkpatel007 <vkpatel007@gmail.com>
2023-03-29 03:45:02 -0400

    Mar-29 Main PC Night

---
+++ 06-jobs-api/starter/controllers/auth.js
@@ -0,0 +1,21 @@
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError } = require('../errors')
+
+
+
+
+const register = async (req, res) => {
+  const user = await User.create({ ...req.body })
+  const token = user.createJWT()
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
+}
+
+const login = async (req, res) => {
+  res.send('login user')
+}
+
+
+
+
+module.exports = { register, login }
---
+++ 06-jobs-api/starter/models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide name'],
+    minLength: 3,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide email'],
+    minLength: 3,
+    maxLength: 50,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please provide a valid email',
+    ],
+    unique: true
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minLength: 6,
+  },
+
+})
+
+UserSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id, name: this.name }, 'jwtSecret', { expiresIn: '30d' })
+}
+
+module.exports = mongoose.model('User', UserSchema)
